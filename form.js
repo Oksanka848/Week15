cb13ea6e36b13aa1153d8ed2a2baa8051b616a41
Oksanaka848 <okmitka@mail.ru>
2022-10-19 22:00:17 +0300

    ср

--- form.js
+++ form.js
@@ -1,5 +1,24 @@
-let input = document.getElementsByTagName ('input').value;
-input.onchange = function() {
-    if (getElementsByTagName ('input').value === '') alert ("Это обязательное поле"); 
-    else alert ('Добро пожаловать, ${name}');
+function init() {
+    let fil = document.getElementById('click').value;
+    fil.onclick = check;}
+    function check() {
+
+ let fil = document.getElementsByTagName ('input').value;
+ let name = document.getElementById ('reg_first_name').value;
+    if (fil.value == '') {
+         alert ("Это обязательное поле"); }
+    else alert(`Привет, ${name} !`);
 }
+
+/*function myFunction() {
+    var inpObj = document.getElementById("id1");
+    if (!inpObj.checkValidity()) {
+        document.getElementById("demo").innerHTML = inpObj.validationMessage;
+    }
+   
+    }
+    function check() {
+        var fil = document.getElementByTagName('input');
+        if (!fil.checkValidity()) {
+            document.getElementByTagName('input').innerHTML = fil.validationMessage;
+        }}*/
