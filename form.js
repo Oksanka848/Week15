d2cc7e94a314c178c4af14ed7cd8c08b1cd3e9f1
Oksanaka848 <okmitka@mail.ru>
2022-10-20 21:50:39 +0300

    ch

--- form.js
+++ form.js
@@ -1,24 +1,58 @@
-function init() {
-    let fil = document.getElementById('click').value;
-    fil.onclick = check;}
-    function check() {
+/*let button = document.getElementById ('check');
+let input = getElementsByTagName ('input').value;
+let name = document.getElementById ('first_name');
+button.onclick = check;
 
- let fil = document.getElementsByTagName ('input').value;
- let name = document.getElementById ('reg_first_name').value;
-    if (fil.value == '') {
-         alert ("Это обязательное поле"); }
-    else alert(`Привет, ${name} !`);
+function check () {
+    if (getElementsByTagName ('input').value == '') 
+    {alert ("Это обязательное поле"); }
+    else {
+        
+        alert ('Добро пожаловать, ${name}');
+    }
 }
+$(document).ready(function() {
+    $('#submit').click(function() {
+        var value = document.getElementById('name').value;
+        if (value === '') {
+            alert('Enter your name');
+        }
+    })
+});*/
+/*let name = document.getElementById ('first_name')[0];
+/*let button = document.getElementById ('check');
 
-/*function myFunction() {
-    var inpObj = document.getElementById("id1");
-    if (!inpObj.checkValidity()) {
-        document.getElementById("demo").innerHTML = inpObj.validationMessage;
+button.onclick = check;
+
+function check () {
+        let value = document.getElementsByTagName ('input').value;
+        if (value === '') {
+            alert('Это обязательное поле');
+        }
+        else {
+            document.getElementsByTagName ('input').style.border = '4em solid black';
+            alert ('Добро пожаловать, ${name}');
+        }
     }
-   
+    /*)
+
+});*/
+function check () {
+let reg_last_name = document.getElementById ('reg_last_name');
+let reg_first_name = document.getElementById ('reg_first_name');
+
+
+document.getElementById ('error')
+.innerHTML=" ";
+
+if (reg_last_name.value == '') {
+document.getElementById ('error')
+.innerHTML+= "Это обязательное поле<br>";
+}
+if (reg_first_name.value == '') {
+    document.getElementById ('error')
+    .innerHTML+= "Это обязательное поле<br>";
+    reg_first_name.value.placeholder = error;
     }
-    function check() {
-        var fil = document.getElementByTagName('input');
-        if (!fil.checkValidity()) {
-            document.getElementByTagName('input').innerHTML = fil.validationMessage;
-        }}*/
+
+}
