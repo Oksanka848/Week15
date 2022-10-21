0ba06566bf3bd4dbc27e99bbc82fde3ba3da1689
Oksanka848 <okmitka@mail.ru>
2022-10-21 14:03:05 +0000

    ch

--- theme/theme.js
+++ theme/theme.js
@@ -1,3 +1,14 @@
 /*Сделайте страничку с возможность выбора темы из выпадающего списка 
 (например, темная, белая, синяя). 
 После выбора темы меняйте фон документа согласно выбранному цвету.*/
+/*function selecttheme() {
+    document.getElementById ('day')
+    .style.backgroundColor = '#fdf7ee';
+    document.getElementById ('day')('#foo').classList.add('myClass');
+}*/
+function selecttheme() {
+document.getElementById ('day').addEventListener('onchange'
+, () => {
+  document.getElementById ('day')('foo').classList.add('myClass');
+});
+}
